c977d663e22e05a22f115a67625164be03c08779
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-04-25 21:27:38 +0430

    Fix Bug

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const container = document.querySelector('.container')
 const range = document.querySelector('#range')
 
 
-range.addEventListener('change',function(event){
+range.addEventListener('input',function(event){
     container.style.filter='brightness('+event.target.value+'%)'
 })
